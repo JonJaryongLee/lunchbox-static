77f22ee5bc51cbdbaebd0dbe610bd0e2a7fd1ba5
JonJaryongLee <director9303@gmail.com>
2021-08-20 19:51:04 +0900

    210820

--- js/login.js
+++ js/login.js
@@ -1,8 +1,37 @@
-const clicktest = async () => {
+const passwordCheck = (userTypedPassword, passwordInDB) => {
+  if (userTypedPassword === passwordInDB) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const showAlarm = (failedAlert) => {
+  failedAlert.style.display = "block";
+  setTimeout(() => {
+    failedAlert.style.display = "none";
+  }, 1000);
+}
+
+const login = async () => {
+  const id = document.querySelector(".id-input").value;
+  const password = document.querySelector(".password-input").value;
+  const failedAlert = document.querySelector(".loginFailedAlert");
   try {
     const response = await axios.get("../test_json/user.json");
     const users = response.data.users;
-    console.log(users);
+    const matchedId = users.filter((user) => user.user_id === id);
+    if (matchedId.length === 0) {
+      showAlarm(failedAlert);
+    } else {
+      const isPasswordCorrect = passwordCheck(password, matchedId[0].password);
+      if (isPasswordCorrect) {
+        // 해당 아이디를 main.js 로 보내야함.
+        location.replace("../main.html");
+      } else {
+        showAlarm(failedAlert);
+      }
+    }
   } catch (error) {
     console.error(error);
   }
