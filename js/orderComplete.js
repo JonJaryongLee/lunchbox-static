f271db6be25ce243f12122d581b98695f40f1a00
JonJaryongLee <director9303@gmail.com>
2021-09-05 14:14:32 +0900

    210905

---
+++ js/orderComplete.js
@@ -0,0 +1,21 @@
+// 세션체크
+onload = async () => {
+    try {
+      const response = await axios.get("../php/sessionCheck.php");
+      if (response.data) {
+        // 세션에 로그인정보가 있을 경우
+        return;
+      } else {
+        // 세션에 로그인정보가 없을 경우
+        location.href = "/html/login.html";
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+  // 3초후 메인화면으로 이동
+  setTimeout(() => {
+    location.href = "/html/main.html";
+  }, 3000);
+  
--- php/getFixOrderInfo.php
+++ php/getFixOrderInfo.php
@@ -1,24 +1,37 @@
 <?php
-// 이 php 파일의 경우, 디비연결부가 불필요함.
-
+require_once("dbconfig.php"); // 항상 맨 앞줄에 추가
 // 정상적인 json 출력을 위해 에러메세지 오프
 error_reporting(0);
 ini_set("display_errors", 0);
-// json type 사용시 필요한 헤더
-header("Content-Type:application/json");
-
-// 세션변수 사용을 위함
-session_start();
+$username = $_SESSION["ses_username"];
+$order_time = $_SESSION["ses_order_time"];
 // 만약 수정버튼을 눌렀을 때 등록된 주문시간이 있다면
 if ($_SESSION["ses_order_time"]) {
-    echo json_encode([
-        'username' => $_SESSION["ses_username"],
-        'order_time' => $_SESSION["ses_order_time"]
-    ],JSON_UNESCAPED_UNICODE|JSON_NUMERIC_CHECK);
+    // 해당 아이디와 주문시간에 일치하는 아이디를 찾는다.
+    $sql = "
+        SELECT 
+            quantity, 
+            address, 
+            what_time, 
+            etc, 
+            menu_day, 
+            menu_name
+        FROM ordered
+        LEFT JOIN menu 
+        ON ordered.menu_id = menu.menu_id
+        WHERE user_id = '$username'
+        AND order_time = '$order_time'
+    ";
+    // 실행결과는 $res에 저장
+    $res = $db->query($sql);
+    // 넘어온 결과를 한 행씩 패치해서 $row 라는 배열에 담는다.
+    $row = $res->fetch_array(MYSQLI_ASSOC); 
+    echo json_encode($row,JSON_UNESCAPED_UNICODE|JSON_NUMERIC_CHECK);
 } else {
     echo json_encode(false,JSON_UNESCAPED_UNICODE|JSON_NUMERIC_CHECK);
 }
-
-unset($_SESSION["ses_order_time"]); // 수정요청 이후엔 필요가 없으니 해제
+// 수정요청 이후엔 필요가 없으니 해제
+unset($_SESSION["ses_order_time"]); 
+mysqli_close($db);
 
 ?>
